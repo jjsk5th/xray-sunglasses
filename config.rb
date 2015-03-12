ff1d5aea0105abb02f939ce4e9ef55db5cd17768
jjsk5th <jjsk5th@Jacks-MacBook-Pro.local>
2015-03-12 15:23:48 -0500

    Initial imports and breakpoints added.
    Module scale and clearfix both imported into project.
    Also first breakpoint added

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 require 'compass/import-once/activate'
-require 'modular-scale'
 # Require any additional compass plugins here.
+require 'modular-scale'
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
