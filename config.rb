c8fdaea6172612e516f26754f184447b3587201f
jjsk5th <jjsk5th@Jacks-MacBook-Pro.local>
2015-03-16 14:11:10 -0500

    Responsive improvements
    Breakpoints and modular scale foundational improvements.

--- config.rb
+++ config.rb
@@ -2,7 +2,6 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 require 'modular-scale'
 
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
