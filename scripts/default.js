52b7cbe30e753fde352cb73bff02c2159eefbfa7
Jack Kent <jack@bright.io>
2015-12-01 09:39:00 -0600

    Fixed header
    Adding fixed header functionality.

--- scripts/default.js
+++ scripts/default.js
@@ -2,3 +2,12 @@ $(document).ready(function (){
 
 
 });
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 800) {
+       $('nav').addClass('fixed-header');
+    }
+    else {
+       $('nav').removeClass('fixed-header');
+    }
+});
