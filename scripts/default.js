b02b08481e7a8a41c97f20c2b26809870e19b521
Jack Kent <jack@bright.io>
2015-12-17 10:29:14 -0600

    makeFixed.js
    Adding makeFixed.js

--- scripts/default.js
+++ scripts/default.js
@@ -1,13 +1,13 @@
 $(document).ready(function (){
-
+	$('nav').makeFixed();
 
 });
 
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 800) {
-       $('nav').addClass('fixed-header');
-    }
-    else {
-       $('nav').removeClass('fixed-header');
-    }
-});
+// $(window).scroll(function(){
+//     if ($(window).scrollTop() >= 800) {
+//        $('nav').addClass('fixed-header');
+//     }
+//     else {
+//        $('nav').removeClass('fixed-header');
+//     }
+// });
